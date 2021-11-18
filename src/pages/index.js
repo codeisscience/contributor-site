038521bca3d617adb25eac9181adda4195e61423
CIPHERTron <pritish.samal918@gmail.com>
2021-11-18 23:51:09 +0530

    feat: Initialize Docusaurus

---
+++ src/pages/index.js
@@ -0,0 +1,57 @@
+import React from "react"
+import clsx from "clsx"
+import Layout from "@theme/Layout"
+import Link from "@docusaurus/Link"
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
+import styles from "./index.module.css"
+import HomepageFeatures from "../components/HomepageFeatures"
+import brand from "../../static/img/brand.png"
+
+function HomepageHeader() {
+	const { siteConfig } = useDocusaurusContext()
+	return (
+		<header
+			className={clsx(
+				"hero hero--primary",
+				styles.heroBanner
+			)}
+		>
+			<div className='container'>
+				<img
+					className={styles.brandImage}
+					src={brand}
+					alt='Code Is Science logo'
+				/>
+				<h1 className={styles.heroTitle}>
+					{siteConfig.title}
+				</h1>
+				<p className='hero__subtitle'>
+					{siteConfig.tagline}
+				</p>
+				<div className={styles.buttons}>
+					<Link
+						className='button button--secondary button--lg'
+						to='/docs/intro'
+					>
+						Getting Started
+					</Link>
+				</div>
+			</div>
+		</header>
+	)
+}
+
+export default function Home() {
+	const { siteConfig } = useDocusaurusContext()
+	return (
+		<Layout
+			title='Community Website'
+			description='Description will go into a meta tag in <head />'
+		>
+			<HomepageHeader />
+			<main>
+				<HomepageFeatures />
+			</main>
+		</Layout>
+	)
+}
